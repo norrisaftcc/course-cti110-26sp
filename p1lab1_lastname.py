18afa9424d683bf5736b317081773b4aefba58c8
teacherbot <norrisa@faytechcc.edu>
2026-02-05 15:52:00 +0000

    finished p1lab! 🐸

--- p1lab1_lastname.py
+++ p1lab1_lastname.py
@@ -4,8 +4,20 @@
 # 2/5/26
 # Practice Python Program
 
-name = "Steve" 
-print ("Nice to meet you", name)
+# name = "Bob"
+# greet the user
+first_name = input("Hi, what's your first name? ")
+last_name = input("What's your last name? ")
+
+print ("Nice to meet you", first_name, last_name)
 print() # extra blank line
+hobby = input("What type of 'stuff' do you like? ")
+print("I'm a computer but I would probably like", hobby, "too.")
+
+# example of a loop
+print("I can repeat myself!")
+count = int(input("How many times should I repeat? "))
+for time in range(count):
+    print("I know a song that drives people crazy,")
 
 
